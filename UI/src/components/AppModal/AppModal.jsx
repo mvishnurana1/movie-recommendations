d9e34aa97ddc9bebc64a97dfe134ffa4490ee843
vish <mvishnurana@gmail.com>
2024-01-29 01:12:14 +1100

    Implemented modal for viewing the chosen film

--- UI/src/components/AppModal/AppModal.jsx
+++ UI/src/components/AppModal/AppModal.jsx
@@ -23,37 +23,51 @@ function AppModal({
                 className={'modal'}
                 isOpen={isModalOpen} 
                 onRequestClose={closeModal}>
-        <div className='flex-top'>
-            <h2 style={{color: 'black'}}>Movie Info:</h2>
-            <button onClick={() => closeModal()}>Close</button>
-        </div>
+        <div className='modal-layout'>
+            <div style={{ width: '50%' }} className='flex-top'>
+                <button className='modal-button'
+                    onClick={() => closeModal()}>
+                    <span>
+                        &#x2715;
+                    </span>
+                </button>
+            </div>
 
-        <div style={{width: '3rem', marginBottom: '4rem' }}>
-            <img src={ backdrop_path ? `${imageBaseUrl}${backdrop_path}` : `${blankFilm}`} alt=""/>
-        </div>
-        <div>
-            {title}
-        </div>
+            <div className='gl-horizontally-centre'> 
+                <img src={ backdrop_path ? `${imageBaseUrl}${backdrop_path}` : `${blankFilm}`} alt={title+'-movie-poster'} width="50%" />
+            </div>
 
-        <div>
-            {numberToMonth(releaseDate.getMonth() + 1)}, {releaseDate.getFullYear()}
-        </div>
+            <div className='half-width' style={{ width: '50%' }}>
+                <div className='gl-horizontal-space-between' style={{ marginTop: '1rem' }}>
+                    <span>
+                        {title}
+                    </span>
 
-        <div>
-            { genre_ids.map((id, index) => <span key={id}>
-                { genreIdToGenreNameMapping[id] + `${isLastElement(index) ? '': ', '}` }
-            </span>) }
-        </div>
+                    <span>
+                        {numberToMonth(releaseDate.getMonth() + 1)}, {releaseDate.getFullYear()}
+                    </span>
+                </div>
 
-        <div>
-            <img src={star} alt="" width="12.8px" height="12.8px" />
-            <span>
-                {vote_average}
-            </span>
-        </div>
+                <div className='gl-horizontal-space-between' style={{ marginTop: '1rem' }}>
+                    <div style={{ width: '70%' }}>
+                        { genre_ids.map((id, index) => <span key={id}>
+                            {`${genreIdToGenreNameMapping[id]}`} 
+                            { isLastElement(index) ? '': <span> &#x2022;</span> } 
+                        </span>) }
+                    </div>
 
-        <div>
-            {overview}
+                    <div>
+                        <img src={star} alt="star-icon" width="12.8px" height="12.8px" />
+                        <span>
+                            {vote_average}
+                        </span>
+                    </div>
+                </div>
+
+                <div style={{ width: '70%', marginTop: '1rem' }}>
+                    {overview}
+                </div>
+            </div>
         </div>
     </ReactModal>
 }

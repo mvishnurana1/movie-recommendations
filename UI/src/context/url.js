94ec4764606c4db0098ba20def673992a4ce6737
vish <mvishnurana@gmail.com>
2024-02-10 21:44:50 +1100

    Made react app ready for deployment

--- UI/src/context/url.js
+++ UI/src/context/url.js
@@ -1,3 +1,9 @@
-const url = 'http://localhost:3002';
+let url = '';
+
+if (process.env.NODE_ENV === 'production') {
+    url = 'http://http://71.19.144.157:3002';
+} else {
+    url = 'http://localhost:3002';
+}
 
 export { url };

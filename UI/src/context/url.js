de4e8ee14fc93f7cd97dd7a49f10b32199f8e6f3
vish <mvishnurana@gmail.com>
2024-02-10 23:26:00 +1100

    Fixed the URl

--- UI/src/context/url.js
+++ UI/src/context/url.js
@@ -1,7 +1,7 @@
 let url = '';
 
 if (process.env.NODE_ENV === 'production') {
-    url = 'http://http://71.19.144.157:3002';
+    url = 'http://71.19.144.157:3002';
 } else {
     url = 'http://localhost:3002';
 }

558a0e02a7524b37c9433c3912a97f9afd00cb02
vish <mvishnurana@gmail.com>
2024-02-12 18:50:44 +1100

    Updated component fonts

--- UI/src/components/ErrorPage/ErrorPage.jsx
+++ UI/src/components/ErrorPage/ErrorPage.jsx
@@ -5,8 +5,8 @@ function ErrorPage() {
 
     return <div className='error-page-container'>
               <img src={ linkUnlink } alt='film-gif' width={'200px'} />
-              <span>Something just went wrong...!</span> 
-              <span>Please refresh the browser</span>
+              <h2 className='gl-header-level-two'>Something just went wrong...!</h2> 
+              <div style={{ fontSize:'20px' }}>Please refresh the browser</div>
           </div>
 }
 
